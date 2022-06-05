4c83d8a4abb49e9b585a00230ad47259852ea95e
Denis <dfgaraev@yandex.ru>
2022-06-05 12:17:40 +0300

    Sprint 3

--- 02_movies_admin/config/settings/components/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from decouple import AutoConfig
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Loading `.env` files
-config = AutoConfig(search_path=BASE_DIR.joinpath('config'))
