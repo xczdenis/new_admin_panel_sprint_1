f7d9ca4e577367844ddd84387429c93c3e70ac80
Denis <dfgaraev@yandex.ru>
2022-06-05 12:18:28 +0300

    Sprint 3

---
+++ 02_movies_admin/config/settings/__init__.py
@@ -0,0 +1,19 @@
+from os import environ
+
+from split_settings.tools import include, optional
+
+environ.setdefault('DJANGO_ENV', 'development')
+_ENV = environ['DJANGO_ENV']
+
+_base_settings = (
+    'components/common.py',
+    'components/database.py',
+
+    # Select the right env:
+    'environments/{0}.py'.format(_ENV),
+
+    # Optionally override some settings:
+    optional('environments/local.py'),
+)
+
+include(*_base_settings)
